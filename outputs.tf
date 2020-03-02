e83f1a11104acb2c9992cedd128b67ef7eab9255
Michael Iglesias <michael@Michaels-MacBook-Air.local>
2020-03-02 11:16:43 -0800

    added outputs

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "vpc-name" {
+  value = module.vpc.name
+}
+
+
+output "vpc-id" {
+  value = module.vpc.default_vpc_id
+}
+
+output "vpc-cidr" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "vpc-subnets-private" {
+  value = module.vpc.private_subnets
+}
+
+output "vpc-subnets-public" {
+  value = module.vpc.public_subnets
+}
+
